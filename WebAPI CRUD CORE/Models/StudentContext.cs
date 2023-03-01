65f6c794d49517aa95b941d04ff2a58d63ad1d28
akhilkpxcoder <akhilkprasannakumar@gmail.com>
2023-03-01 16:06:27 +0530

    New update

---
+++ WebAPI CRUD CORE/Models/StudentContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI_CRUD_CORE.Models
+{
+    public class StudentContext :DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get;set; }
+    }
+}
