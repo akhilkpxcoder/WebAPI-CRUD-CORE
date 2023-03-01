4ffc4ead56e9f9274af5aef0f99da93f9fac7c61
akhil.kp <akhil.kp@SYSLP1251>
2023-03-01 15:28:51 +0530

    Add project files.

---
+++ WebAPI CRUD CORE/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI_CRUD_CORE.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public DateOnly DOB { get; set; }
+
+    }
+}
