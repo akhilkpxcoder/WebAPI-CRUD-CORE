f5fd64c81412bfaafc0fac214771b0a4bedc0367
akhilkpxcoder <akhilkprasannakumar@gmail.com>
2023-03-02 16:23:37 +0530

    New  api request added

--- WebAPI CRUD CORE/Controllers/StudentController.cs
+++ WebAPI CRUD CORE/Controllers/StudentController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI_CRUD_CORE.Models;
 
 namespace WebAPI_CRUD_CORE.Controllers
 {
@@ -7,5 +9,30 @@ namespace WebAPI_CRUD_CORE.Controllers
     [ApiController]
     public class StudentController : ControllerBase
     {
+        private readonly StudentContext _dbContext;
+
+        public StudentController(StudentContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudent()
+        {
+            if(_dbContext.Students == null) 
+            { 
+                return NotFound();
+            }
+            return await _dbContext.Students.ToListAsync();
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudent(int id)
+        {
+            if (_dbContext.Students == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Students.ToListAsync();
+        }
     }
 }
