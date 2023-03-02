1016279beec83a17606597d4365989e711b3f2d5
akhilkpxcoder <akhilkprasannakumar@gmail.com>
2023-03-02 16:47:47 +0530

    Change in model class and added Post Request

--- WebAPI CRUD CORE/Controllers/StudentController.cs
+++ WebAPI CRUD CORE/Controllers/StudentController.cs
@@ -25,14 +25,27 @@ namespace WebAPI_CRUD_CORE.Controllers
             }
             return await _dbContext.Students.ToListAsync();
         }
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Student>>> GetStudent(int id)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
         {
             if (_dbContext.Students == null)
             {
                 return NotFound();
             }
-            return await _dbContext.Students.ToListAsync();
+            var student = await _dbContext.Students.FindAsync(id);
+            if(student == null)
+            {
+                return NotFound();
+            }
+            return student;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Student>> PostStudent(Student student)
+        {
+            _dbContext.Students.Add(student);   
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetStudent), new {id = student.ID},student);
         }
     }
 }
--- WebAPI CRUD CORE/Models/Student.cs
+++ WebAPI CRUD CORE/Models/Student.cs
@@ -5,7 +5,7 @@
         public int ID { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
-        public DateTime DOB { get; set; }
+        public string DOB { get; set; }
         public int Age { get; set; }
 
     }
