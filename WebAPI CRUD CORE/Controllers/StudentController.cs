2c96426c62896c71e732a0a2308b3a239bb93ec1
akhilkpxcoder <akhilkprasannakumar@gmail.com>
2023-03-02 15:54:27 +0530

    new conroller

---
+++ WebAPI CRUD CORE/Controllers/StudentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_CRUD_CORE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+    }
+}
