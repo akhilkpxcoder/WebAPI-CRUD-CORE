e3edff83dd21e13582b044168770521c84ea8c05
Akhil KP <98999402+akhilkpxcoder@users.noreply.github.com>
2023-03-09 00:13:53 -0500

    new update

--- WebAPI CRUD CORE/Models/Student.cs
+++ WebAPI CRUD CORE/Models/Student.cs
@@ -3,10 +3,18 @@
     public class Student
     {
         public int ID { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string DOB { get; set; }
-        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone_Number { get; set; }
+        public string Username { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
 
     }
 }
